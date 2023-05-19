2801ec596979863c5cb6cba29a00401ae59c7217
Vidocq <vidocqho@gmail.com>
2023-05-19 22:00:31 +0800

    feat: ios support

--- ios/ExpoReferenceLibModule.swift
+++ ios/ExpoReferenceLibModule.swift
@@ -1,44 +1,36 @@
 import ExpoModulesCore
 
 public class ExpoReferenceLibModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoReferenceLib')` in JavaScript.
-    Name("ExpoReferenceLib")
-
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
+    // Each module class must implement the definition function. The definition consists of components
+    // that describes the module's functionality and behavior.
+    // See https://docs.expo.dev/modules/module-api for more details about available components.
+    public func definition() -> ModuleDefinition {
+        var referenceVC : UIReferenceLibraryViewController =  UIReferenceLibraryViewController(term: "")
+        // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
+        // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
+        // The module will be accessible from `requireNativeModule('ExpoReferenceLib')` in JavaScript.
+        Name("ExpoReferenceLib")
+        
+        // Defines a JavaScript function that always returns a Promise and whose native code
+        // is by default dispatched on the different thread than the JavaScript runtime runs on.
+        AsyncFunction("presentReferenceLibraryAsync") { (value: String) in
+            DispatchQueue.main.async {
+                referenceVC = UIReferenceLibraryViewController(term: value)
+                let rootVC = UIApplication.shared.keyWindow?.rootViewController
+                rootVC?.present(referenceVC, animated: true)
+            }
+        }
+        
+        AsyncFunction("dismissReferenceLibraryAsync") {
+            DispatchQueue.main.async {
+                referenceVC.dismiss(animated: true)
+            }
+        }
+        
+        //      Function("dictionaryHasDefinition") { (value: String) in
+        //          DispatchQueue.main.async {
+        //              UIReferenceLibraryViewController.dictionaryHasDefinition(forTerm: value)
+        //          }
+        //      }
     }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoReferenceLibView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: ExpoReferenceLibView, prop: String) in
-        print(prop)
-      }
-    }
-  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
 // Import the native module. On web, it will be resolved to ExpoReferenceLib.web.ts
 // and on native platforms to ExpoReferenceLib.ts
-import ExpoReferenceLibModule from './ExpoReferenceLibModule';
-import ExpoReferenceLibView from './ExpoReferenceLibView';
-import { ChangeEventPayload, ExpoReferenceLibViewProps } from './ExpoReferenceLib.types';
-
-// Get the native constant value.
-export const PI = ExpoReferenceLibModule.PI;
-
-export function hello(): string {
-  return ExpoReferenceLibModule.hello();
+import ExpoReferenceLibModule from "./ExpoReferenceLibModule";
+export async function presentReferenceLibraryAsync(value: string) {
+  return await ExpoReferenceLibModule.presentReferenceLibraryAsync(value);
 }
 
-export async function setValueAsync(value: string) {
-  return await ExpoReferenceLibModule.setValueAsync(value);
+export async function dismissReferenceLibraryAsync() {
+  return await ExpoReferenceLibModule.dismissReferenceLibraryAsync();
 }
-
-const emitter = new EventEmitter(ExpoReferenceLibModule ?? NativeModulesProxy.ExpoReferenceLib);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoReferenceLibView, ExpoReferenceLibViewProps, ChangeEventPayload };
