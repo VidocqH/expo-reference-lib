e5d8a437948f2255202c625dab1775f33721185d
Vidocq <vidocqho@gmail.com>
2023-05-19 22:00:54 +0800

    feat: example project

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ExpoReferenceLib from 'expo-reference-lib';
+import * as ExpoReferenceLib from "expo-reference-lib";
+import { useState } from "react";
+import { StyleSheet, Button, View, TextInput } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const press = () => {
+    ExpoReferenceLib.presentReferenceLibraryAsync(text);
+    setTimeout(() => {
+      ExpoReferenceLib.dismissReferenceLibraryAsync(); // IOS only
+    }, 5000);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{ExpoReferenceLib.hello()}</Text>
+      <TextInput value={text} onChangeText={setText} style={styles.input} />
+      <Button title="Look Up" onPress={press} />
     </View>
   );
 }
@@ -13,8 +23,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "80%",
+    height: 30,
+    borderWidth: 1,
   },
 });
